a29c9db63f224a3bc4cdc4d975b6ffadee228a8e
Carlos Henrique <carloshpds@gmail.com>
2015-12-26 17:47:39 -0200

    [Utils] Using chalk by options.plugins.chalk -- Bringing functions to inside of module

--- dist/utils.js
+++ dist/utils.js
@@ -1,40 +1,4 @@
 
-var defaultOptions = null;
-var options        = null;
-var chalk          = require('chalk');
-
-function requireModule (key) {
-  var moduleName = options.modulesData[key].uses;
-  var data       = options.modulesData[key];
-  var module     = null;
-
-  if(data.isDefault){
-    module = require(moduleName);
-  } else {
-    module = require(process.cwd() + moduleName);
-  }
-
-  return module;
-}
-
-function getBaseBuildName(onlyText){
-  var text =  chalk.white('[ ' + 'Base Build ' + chalk.underline.red('Angular') + ' ]')  + ' ';
-
-  if(onlyText){
-    text = chalk.white( 'Base Build ' + chalk.underline.red('Angular') );
-  }
-
-  return text;
-}
-
-function getTimeoutZeroName(){
-  return chalk.white('Timeout') + chalk.cyan('Zero');
-}
-
-function getRedsparkName(){
-  return chalk.white( chalk.red('red')  + 'spark' )
-}
-
 
 /*
   ==========================
@@ -46,8 +10,51 @@ module.exports = function(options) {
   /*
    * Required resources
    */
-  defaultOptions = options.defaultOptions;
-  options        = options;
+  var defaultOptions = options.defaultOptions;
+  var chalk          = options.plugins.chalk;
+
+
+
+  /*
+   * Methods
+   */
+
+  /**
+   * Requires a basebuild module
+   * @param  {String} key module name
+   * @return {Function} module
+   */
+  function requireModule (key) {
+    var moduleName = options.modulesData[key].uses;
+    var data       = options.modulesData[key];
+    var module     = null;
+
+    if(data.isDefault){
+      module = require(moduleName);
+    } else {
+      module = require(process.cwd() + moduleName);
+    }
+
+    return module;
+  }
+
+  function getBaseBuildName(onlyText){
+    var text =  chalk.white('[ ' + 'Base Build ' + chalk.underline.red('Angular') + ' ]')  + ' ';
+
+    if(onlyText){
+      text = chalk.white( 'Base Build ' + chalk.underline.red('Angular') );
+    }
+
+    return text;
+  }
+
+  function getTimeoutZeroName(){
+    return chalk.white('Timeout') + chalk.cyan('Zero');
+  }
+
+  function getRedsparkName(){
+    return chalk.white( chalk.red('red')  + 'spark' )
+  }
 
   return {
     requireModule      : requireModule,
